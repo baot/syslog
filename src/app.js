ce4b228a542f2f382b3408276c60cd27ec8c2364
Tran Bao <tran.bao51093@gmail.com>
2016-10-27 12:41:23 +0300

    add nedb

--- src/app.js
+++ src/app.js
@@ -2,11 +2,36 @@
 
 import http from 'http';
 import express from 'express';
+import Datastore from 'nedb';
 import { argv } from 'optimist';
-
 
 const syslogPort = argv.s;
 const apiPort = argv.r;
+
+/*
+ * DATABASE
+ */
+
+const db = new Datastore({
+  filename: 'syslog.db', // path to data file,
+  autoload: true,  // automatically load the db
+});
+
+//
+const syslogMessage = {
+  time: '2014-09-01',
+  ip: '172.16.100.162',
+  cat: 'NETCON',
+  event: 'init_complete',
+  action: null,
+  message: null,
+};
+
+db.insert(syslogMessage, (err, msg) => {
+  if (err) console.error(err);
+  console.info(msg);
+});
+
 
 /*
  * SERVER
